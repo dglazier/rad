65cf75a88a13508d81098784b4847754c2498cc6
Derek Glazier <Derek.Glazier@glasgow.ac.uk>
2025-05-07 09:51:42 +0100

    add Resolution functions to ReactionUTils

--- include/ReactionUtilities.h
+++ include/ReactionUtilities.h
@@ -1,27 +1,30 @@
 #pragma once
 
 //#include "ConfigReaction.h"
+#include "DefineNames.h"
+
 #include <TString.h>
 #include <ROOT/RDFHelpers.hxx>
 
 namespace rad{
   namespace reaction{
     namespace util{
-
+      
+      
     
       // void CountParticles(rad::config::ConfigReaction* rad, const std::string& type){
       template<typename T> //use template so can #include this in ConfigReaction
-      void CountParticles(T* rad, const std::string& type){
+      void CountParticles(T* rdf, const std::string& type){
       
-	rad->Define(type+"Ngamma",Form("rad::helpers::Count(%spid,22)",type.data()) );
-	rad->Define(type+"Npip",Form("rad::helpers::Count(%spid,211)",type.data()) );
-	rad->Define(type+"Npim",Form("rad::helpers::Count(%spid,-211)",type.data()) );
-	rad->Define(type+"NKp",Form("rad::helpers::Count(%spid,321)",type.data()) );
-	rad->Define(type+"NKm",Form("rad::helpers::Count(%spid,-321)",type.data()) );
-	rad->Define(type+"Nele",Form("rad::helpers::Count(%spid,11)",type.data()) );
-	rad->Define(type+"Npos",Form("rad::helpers::Count(%spid,-11)",type.data()) );
-	rad->Define(type+"Npro",Form("rad::helpers::Count(%spid,2212)",type.data()) );
-	rad->Define(type+"Nneutron",Form("rad::helpers::Count(%spid,2112)",type.data()) );
+	rdf->Define(type+"Ngamma",Form("rad::helpers::Count(%spid,22)",type.data()) );
+	rdf->Define(type+"Npip",Form("rad::helpers::Count(%spid,211)",type.data()) );
+	rdf->Define(type+"Npim",Form("rad::helpers::Count(%spid,-211)",type.data()) );
+	rdf->Define(type+"NKp",Form("rad::helpers::Count(%spid,321)",type.data()) );
+	rdf->Define(type+"NKm",Form("rad::helpers::Count(%spid,-321)",type.data()) );
+	rdf->Define(type+"Nele",Form("rad::helpers::Count(%spid,11)",type.data()) );
+	rdf->Define(type+"Npos",Form("rad::helpers::Count(%spid,-11)",type.data()) );
+	rdf->Define(type+"Npro",Form("rad::helpers::Count(%spid,2212)",type.data()) );
+	rdf->Define(type+"Nneutron",Form("rad::helpers::Count(%spid,2112)",type.data()) );
       }
     
       //////////////////////////////////////////////////////////////////
@@ -35,6 +38,29 @@ namespace rad{
 	toString.pop_back(); //remove last ,
 	toString+='}';
 	return toString;
+      }
+    
+
+      using rad::names::data_type::Rec;
+      using rad::names::data_type::Truth;
+
+      /**
+       * calculate the difference in reconsutructed and truth variables
+       * Case Reconstructed and truth synched via AliasColumnsAndMatchWithMC()
+       */
+      template<typename T> //use template so can #include this in ConfigReaction
+      void Resolution(T* const  rdf,const string& var){
+	// Define(string("res_")+var,[](const ROOT::RVec<T> &rec,const ROOT::RVec<T> &tru){
+	//   return (rec - tru);
+	// },{string(Rec())+var,string(Truth())+var});
+	rdf->Define(string("res_")+var,Form("%s-%s",(Truth()+var).data(),(Rec()+var).data() ));
+      }
+      template<typename T> //use template so can #include this in ConfigReaction
+      void ResolutionFraction(T* const rdf,const string& var){
+	// Define(string("res_")+var,[](const ROOT::RVec<T> &rec,const ROOT::RVec<T> &tru){
+	//    return (rec - tru)/tru;
+	// },{Rec()+var,Truth()+var});
+	rdf->Define(string("res_")+var,Form("(%s-%s)/%s",(Truth()+var).data(),(Rec()+var).data(),(Truth()+var).data() ));
       }
     }
   }//reaction
