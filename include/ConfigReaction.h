3f29a66fb548d49fc1f75522ee8a5130d72e4c50
Derek Glazier <Derek.Glazier@glasgow.ac.uk>
2025-05-30 10:15:54 +0100

    additional operations on columns names

--- include/ConfigReaction.h
+++ include/ConfigReaction.h
@@ -42,9 +42,23 @@ namespace rad{
       }
       cout<<"\n";
     }
+   void PrintAllColumnNames(RDFstep  df){
+      std::cout<<"Print Column Names : ";
+      auto cols =  df.GetColumnNames();
+      for(auto& col:cols){
+	std::cout<<col<<", ";
+      }
+      cout<<"\n";
+    }
 
     bool ColumnExists(const string& col,RDFstep  df){
       auto cols =  df.GetDefinedColumnNames();
+      if(std::find(cols.begin(),cols.end(),col)==cols.end()) return false;
+      return true;
+    }
+    
+    bool OriginalColumnExists(const string& col,RDFstep  df){
+      auto cols =  df.GetColumnNames();
       if(std::find(cols.begin(),cols.end(),col)==cols.end()) return false;
       return true;
     }
