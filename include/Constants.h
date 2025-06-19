420697e7e569b027b47819e6bf3100518078e652
Derek Glazier <Derek.Glazier@glasgow.ac.uk>
2025-06-19 12:49:01 +0100

    make IsInvalidEntry tempalted function

--- include/Constants.h
+++ include/Constants.h
@@ -27,6 +27,10 @@ namespace rad{
       return std::numeric_limits<unsigned int>::max();
     }
 
+    template<typename T>
+    inline bool IsInvalidEntry(const T& entry){
+      return entry==InvalidEntry<T>();
+    }
     // constexpr double InvalidEntry(){return std::numeric_limits<double>::quiet_NaN();};
     constexpr int InvalidIndex(){return -1;};
 
